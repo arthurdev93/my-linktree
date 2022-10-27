938c5d32375f6516bcadb78356ce4db1d0fe83ed
ARTHUR DE MATOS ELSNER <arthurdev93@gmail.com>
2022-10-27 18:30:36 -0300

    ++

---
+++ main.js
@@ -0,0 +1,12 @@
+//Função efeito mouse em cima dos botões
+function onOver(li) {
+    li.style.filter = "saturate(10)";
+}
+
+//função para a saída do mousa, abaixo
+function onOut(li) {
+    li.style.filter = "none";
+}
+
+//tentar com objeto, array, ou DOM
+
