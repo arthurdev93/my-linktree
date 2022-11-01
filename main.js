35e5d786417000bd8ea55cbe8542cd8e8e0f1e76
ARTHUR DE MATOS ELSNER <arthurdev93@gmail.com>
2022-11-01 19:18:13 -0300

    ++

--- main.js
+++ main.js
@@ -1,14 +1,12 @@
 //Função efeito mouse em cima dos botões
 function onOver(li) {
-    li.style.filter = "saturate(10)";
+    li.style.color = "#005179";
 }
 
 //função para a saída do mousa, abaixo
 function onOut(li) {
-    li.style.filter = "none";
+    li.style.color = "#00111B";
 }
-//efeito MATRIX background
 
-//tentar com objeto, array, ou DOM, otimizar no html, para não repetir os atributos todas as linhas
 
 
--- matrix.js
+++ matrix.js
@@ -1,6 +1,8 @@
 //criação de efeito matrix com minha imagem de background. >>ver algum efeito de movimento
 /*
+
 function matrix(img) {
-    img.style.zoom = "110%"
+    img.style.transform.transition = "linear"
 }
+
 */
